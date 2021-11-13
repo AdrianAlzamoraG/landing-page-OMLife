151418da424637e834f326227c086dcf9e356993
AdrianAlzamoraG <u202015385@upc.edu.pe>
2021-11-13 15:51:47 -0500

    Modificacion style.css

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,3 +3,7 @@ let hNav = document.getElementById('header-nav');
 btnMenu.addEventListener('click', function(){
   hNav.classList.toggle('mostrar');
 });
+
+function bimg(){
+  document.getElementsByClassName('banner').style.backgroundImage = "url(assets/img/banner.jpg)";
+}
